a044637f8f55b0b542cc6e1542886751af22b565
Igor Krasilnikov <kite84nsk@gmail.com>
2025-10-14 09:43:42 +0700

    Create Changing letters.js

---
+++ Changing letters.js
@@ -0,0 +1,11 @@
+/*
+When provided with a String, capitalize all vowels
+For example:
+Input : "Hello World!"
+Output : "HEllO WOrld!"
+Note: Y is not a vowel in this kata.
+*/
+
+function swap (string) {
+  return string.replace(/[aeiou]/g, elem => elem.toUpperCase());
+}
