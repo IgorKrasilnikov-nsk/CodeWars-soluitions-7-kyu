3fda25b51a281227f2517d740847a9bbfcfa48e8
Igor Krasilnikov <kite84nsk@gmail.com>
2025-10-21 08:33:01 +0700

    Create isEven? - Bitwise Series.js

---
+++ isEven? - Bitwise Series.js
@@ -0,0 +1,13 @@
+/*
+Is the number even?
+If the numbers is even return true. If it's odd, return false.
+Oh yeah... the following symbols/commands have been disabled!
+
+use of %
+use of .even? in Ruby
+use of mod in Python
+*/
+
+var isEven = function (n) { // if n is even return true, otherwise, return false
+  return ((n & 1) === 0) ? true : false;
+}
